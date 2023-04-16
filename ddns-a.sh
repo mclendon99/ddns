82354ebc6f334d9d60d40fd242aaf299e37bf538
McLendon <postmaster@bellsouth.net>
2023-04-16 15:05:00 -0400

    Lot's of extra checking in ddns-a.sh, Emptied ddns.conf as it was overwriting the env.

--- ddns-a.sh
+++ ddns-a.sh
@@ -5,20 +5,37 @@
 #set -x
 
 # Support env variables as well as config file
-[ -f ./ddns.conf ] && . ./ddns.conf
-
-if [[ -z $GDAPIKEY || -z $MYDOMAIN || -z $GODADDYHOST ]]
+if [[ -f ./ddns.conf ]]
 then
-    echo "Must supply GDAPIKEY, MYDOMAIN and GODADDYHOST variables in environment variables or ddns.conf file. Exiting."
+    . ./ddns.conf 
+    echo "Using GDAPIKEY, MYDOMAIN, GODADDYHOST from ddns.conf."
+fi
+# Check that something is there
+if [[ -z $GDAPIKEY ]]
+then
+    echo "Must supply GDAPIKEY variable in environment variables or ddns.conf file. Exiting."
+    exit -2
+elif [[ -z $MYDOMAIN  ]]
+then
+    echo "Must supply MYDOMAIN variable in environment variables or ddns.conf file. Exiting."
+    exit -2
+elif [[ -z $GODADDYHOST ]]
+then
+    echo "Must supply GODADDYHOST variable in environment variables or ddns.conf file. Exiting."
     exit -2
 fi
 # Check the domain exists
+echo "Checking domain: ${MYDOMAIN}"
 resp=`curl -s -X GET "${GODADDYHOST}/v1/domains/${MYDOMAIN}"  -H "Authorization: sso-key ${GDAPIKEY}"`
 if grep -q "NOT_FOUND" <<< $resp ; then
     echo "Domain ${MYDOMAIN} does not appear to exist on GoDaddy. Exiting."
     exit -3
+elif grep -q "UNABLE_TO_AUTHENTICATE" <<< $resp ; then
+    echo "GDAPIKEY failed authentication. Exiting."
+    exit -3
 fi
 # External IP
+echo "Retrieving A record"
 myip=`curl -s "https://api.ipify.org"`
 dnsdata=`curl -s -X GET -H "Authorization: sso-key ${GDAPIKEY}" "${GODADDYHOST}/v1/domains/${MYDOMAIN}/records/A"`
 gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
@@ -27,7 +44,7 @@ echo "`date '+%Y-%m-%d %H:%M:%S'` - Current external IP is $myip. GoDaddy DNS IP
 if [[ "$gdip" != "$myip" && "$myip" != "" ]]; then
   echo "IP has changed! Updating on GoDaddy..."
   resp=`curl -s -X PUT "${GODADDYHOST}/v1/domains/${MYDOMAIN}/records/A/@" -H "Authorization: sso-key ${GDAPIKEY}" -H "Content-Type: application/json" -d "[ {\"data\":\"${myip}\",\"name\":\"@\",\"port\":65535,\"ttl\":3600,\"type\":\"A\"} ]"`
-  echo "Response is: ${resp}"
+  echo "Response to PUT is: ${resp}"
   echo "Changed A record IP on ${MYDOMAIN} from ${gdip} to ${myip}."
 else
   echo "IP has not changed! No action taken."
