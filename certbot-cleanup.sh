2773ed30c43a9111e2827a36181ecc6daea16b0f
McLendon <postmaster@bellsouth.net>
2023-12-17 11:03:59 -0500

    More cleanup. Eliminate duplicate script

--- certbot-cleanup.sh
+++ certbot-cleanup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# This script delete all _acme-challenge TXT records on godaddy for GDDOMAIN.
+# This script deletes all _acme-challenge TXT records on godaddy for GDDOMAIN.
 #set -x
-if [[ -z $CERTBOT_DOMAIN ]]
+if [[ -z $GDDOMAIN ]]
 then
-    echo "Must supply an CERTBOT_DOMAIN environment variable. Exiting."
+    echo "Must supply an GDDOMAIN environment variable. Exiting."
     exit -1
 fi
-
-GDDOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 
 # Support env variables as well as config file
 # Check the args
@@ -15,7 +13,7 @@ if [[ -z $GDAPIKEY ]]
 then
     echo "Must supply GDAPIKEY in environment variable or ddns.conf file."
 fi
-fi
+
 if [[ -z $GDHOST ]]
 then
     echo "Must supply GDHOST, e.g. https://api.godaddy.com, in environment variable."
@@ -28,7 +26,7 @@ then
 fi
 
 echo "Deleting ACME challenges for ${GDDOMAIN}on ${GDHOST}."
-resp=`curl -s -X DELETE "${GDHOST}/v1/domains/${GDDOMAIN}/records/TXT/_acme-challenge" -H "Authorization: sso-key ${GDAPIKEY}" 
+resp=`curl -s -X DELETE "${GDHOST}/v1/domains/${GDDOMAIN}/records/TXT/_acme-challenge" -H "Authorization: sso-key ${GDAPIKEY}"`
 if grep -q "NOT_FOUND" <<< $resp ; then
     echo "Domain ${GDDOMAIN} does not appear to exist on ${GDHOST}. Exiting."
     exit -3
